786a278e2e825df6518eb28533786f98dff82e6a
mlenkeit <maximilian.lenkeit@gmail.com>
2015-03-28 18:40:43 +0100

    adds basic auth to capture credentials

--- test/proxy.js
+++ test/proxy.js
@@ -1,3 +1,31 @@
+var express = require('express');
+var request = require('supertest');
+var proxy = require('./../lib/proxy');
+
+var middleware;
+var app;
+var host;
+var serviceEndpoint;
+
 describe('proxy', function() {
+
+	describe('with host', function() {
+
+		beforeEach(function() {
+			host = 'https://hanaxs.ns.ondemand.com';
+			serviceEndpoint = '/account/package/service.xsjs'
+			middleware = proxy(host);
+			app = express();
+			app.use('/', middleware);
+		});
+
+		it('should request basic-auth when sending a request for the first time', function(done) {
+			request(app)
+				.get(serviceEndpoint)
+				.expect('WWW-Authenticate', 'Basic realm="' + host + '"')
+				.expect(401, done);
+		});
+
+	});
 
 });
